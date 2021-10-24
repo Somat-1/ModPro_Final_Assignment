3ec047bf323d5eef3adbdcfee807ea9d3063054d
Somat-1 <68004992+Somat-1@users.noreply.github.com>
2021-10-24 18:17:01 +0200

    Update truss_definition.m

--- truss_definition.m
+++ truss_definition.m
@@ -1,12 +1,12 @@
 % Assignment 1; statistically determinable truss
 % Truss definition script to define the truss
 
-collect_data
+% collect_data ; gets the joint coordinates by user input (S)
 
 
 % jointCoordinates - 4 by 2 matrix, with the x-coordinates in the first column and
 % the y-coordinates in the second column
-    jointCoordinates = S;
+    jointCoordinates = [0 0; 0.5 -0.6; 1.2 -0.7; 1 0];
 
     
 %  by 2 matrix, with each row containing the joint numbers that are
@@ -17,13 +17,13 @@ collect_data
 %  4 by 2 matrix. Each row contains one reaction force, with the
 %  first column representing the joint number, and the second column the direction of
 % the reaction force (1 for x-direction and 2 for y-direction).
-    supportReactions = 0;
+    supportReactions = [1 -1; 1 -2; 4 -1; 4 -2];
     
     
 %  by 3 matrix.  Each row contains one external force, withthe first column representing
 % the joint number, the second column the direction ofthe external force 
 % (1 for x-direction and 2 for y-direction) and the third column themagnitude of the external force.
-    externalForces = 0 ;
+    externalForces = [2 2 -100; 3 1 -50; 3 2 30] ;
     
     
     
