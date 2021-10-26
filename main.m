dcf54459b9f7ccedd4646325e6f7657abfe7d315
GabrielPals <93044519+GabrielPals@users.noreply.github.com>
2021-10-26 12:18:59 +0200

    Add files via upload

--- main.m
+++ main.m
@@ -1,35 +1,48 @@
-% Assignment 1; statistically determinable truss
-% Main script file for all the calculations 
+%% ModPro - Final Assessment - Main file
+%Asvaldur Gabriel Moura Palsson
+%29/10/2021
 
+%% clearing the command window and the variables
+clc, clear
 
-% runs the script "truss_definition"
-truss_definition
+%% i) Running the truss initialization file
+truss_definition;
 
-% checking if the truss is statistically determinable:
-% number of joints noted nJoints
-[m,n] = size(jointCoordinates);
-nJoints = m;
+%% ii) Determining whether or not the system is statically determinate
+% Defining variables
+nJoints=size(jointCoordinates,1);
+nTrusses=size(trusses,1);
+nSupports=size(supportReactions,1);
 
-% number of trusses noted nTrusses
-[m,n] = size(trusses);
-nTrusses = m;
+numberOfUnknowns=(nTrusses+nSupports);
+numberOfEquations=(2*nJoints);
 
-% number of reaction forces notet nSupport
-[m,n] = size(supportReactions);
-nSupports = m;
-
-% number of unknowns and number of equations, with their respective names:
-numberOfUnknowns = nTrusses + nSupports;
-numberOfEquations = 2*nJoints;
-
-% condition to disp an error if the truss is statistically over- or under-
-% determinable
-if (numberOfEquations ~= numberOfUnknowns)
-   error('Defined truss system is not statically determinate');
+% Finding if it is statically determinate
+if(numberOfUnknowns ~= numberOfEquations)
+    error('Defined truss system is not statically determinate');
 end
 
-A = zeros(numberOfEquations,numberOfUnknowns);
+%% iii) Initializing matrix A 
+A=zeros(numberOfEquations,numberOfUnknowns);
 
+%% iv)
+for iTruss=1:nTrusses
+% a) defining iJoints and jJoints
+iJoint=trusses(iTruss,1);
+jJoint=trusses(iTruss,2);
 
-    
-    
+% b) Calculating deltaX and deltaY
+deltaX=jointCoordinates(jJoint,1)-jointCoordinates(iJoint,1);
+deltaY=jointCoordinates(jJoint,2)-jointCoordinates(iJoint,2);
+
+% c) Calculating the length of the truss
+L= sqrt((deltaX)^2+(deltaY)^2);
+
+% d) Assigning the di
+A(iJoint,iTruss)=deltaX/L;
+A(jJoint,iTruss)=-deltaX/L;
+A(iJoint+nJoints,iTruss)=deltaY/L;
+A(jJoint+nJoints,iTruss)=-deltaY/L;
+end
+
+%% v) 
--- truss_definition.m
+++ truss_definition.m
@@ -1,31 +1,21 @@
-% Assignment 1; statistically determinable truss
-% Truss definition script to define the truss
+% ModPro - Final Assessment - 
+% Asvaldur Gabriel Moura Palsson
+% 29/10/2021
 
-% collect_data ; gets the joint coordinates by user input (S)
+%% i) Defining the variables
 
+% a) Coordinates Matrix: the coordinates of the joints, where column 1 is the
+% x-coordinates and column 2 is the y-coordinates
+jointCoordinates= [0 0; 0.5 -0.6; 1.2 -0.7; 1 0];
 
-% jointCoordinates - 4 by 2 matrix, with the x-coordinates in the first column and
-% the y-coordinates in the second column
-    jointCoordinates = [0 0; 0.5 -0.6; 1.2 -0.7; 1 0];
+% b) Trusses matrix: Trusses and the joints that they connect
+trusses= [1 2; 2 4; 2 3; 3 4];
 
-    
-%  by 2 matrix, with each row containing the joint numbers that are
-% connected by each truss.
-    trusses = [1 2; 2 3; 2 4; 3 4];
-    
-    
-%  4 by 2 matrix. Each row contains one reaction force, with the
-%  first column representing the joint number, and the second column the direction of
-% the reaction force (1 for x-direction and 2 for y-direction).
-    supportReactions = [1 -1; 1 -2; 4 -1; 4 -2];
-    
-    
-%  by 3 matrix.  Each row contains one external force, withthe first column representing
-% the joint number, the second column the direction ofthe external force 
-% (1 for x-direction and 2 for y-direction) and the third column themagnitude of the external force.
-    externalForces = [2 2 -100; 3 1 -50; 3 2 30] ;
-    
-    
-    
-    
-    
+% c) Reaction forces Matrix: 
+% The joint on which the support reaction acts (column 1) and the direction of the force (column 2, with 1=x-direction and 2=y-direction)
+supportReactions= [1 1; 1 2; 4 1; 4 2];
+
+% d) The external forces Matrix, where the first s=column is yhe joint
+% number, the second column is the direction a=of the force and the the
+% third column if the magnitude of the external forces
+externalForces= [2 2 -100; 3 1 -50; 3 2 30];
